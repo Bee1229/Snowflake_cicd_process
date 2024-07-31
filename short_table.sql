10ada64a2afe36cfbe4a885ed7181724a412a523
Blessing Torti <123904703+Bee1229@users.noreply.github.com>
2024-07-31 13:51:57 +0200

    Update short_table.sql

--- short_table.sql
+++ short_table.sql
@@ -9,3 +9,9 @@ VALUES
 (1001, 'Tom', 'Erichsen'),
 (1002, 'Per', 'Olsen'),
 (1003, 'Finn', 'Egan');
+
+
+
+CREATE ROLE IF NOT EXISTS ROLE_CLARA;
+COMMENT ON ROLE ROLE_CLARA IS 'Primary role assigned to CLARK_KENT';
+GRANT ROLE ROLE_CLARA TO ROLE ACCOUNTADMIN;
